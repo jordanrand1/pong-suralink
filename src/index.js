fe2b163372fb7d0e29d67a47c2e712ef90518ab8
Jordan Rand <jorrand@gmail.com>
2018-10-19 06:35:38 -0600

    fixed index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+import GameInterface from "./GameInterface";
+import * as serviceWorker from "./serviceWorker";
 
-import "./styles.css";
+ReactDOM.render(<GameInterface />, document.getElementById("root"));
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+serviceWorker.unregister();
